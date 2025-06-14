4f7dc0354258bfc248de5f7ba461455af460a11d
Gowreesh A <gowreesha@Gowreeshs-MacBook-Air.local>
2025-06-14 15:22:32 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './components/LoginPage';
 import CodingDebugQuiz from './components/CodingDebugQuiz';
 
-const App = () => {
+function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LoginPage />} />
-        <Route path="/code-debug-quiz" element={<CodingDebugQuiz />} />
+        <Route path="/quiz" element={<CodingDebugQuiz />} />
       </Routes>
     </Router>
   );
-};
+}
 
 export default App;
